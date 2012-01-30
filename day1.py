52983d9affe21bb54fafba9c1e1d514fc9f4e629
Keenan <kmv11@hampshire.edu>
2012-01-30 11:38:38 -0500

    Class Today

---
+++ day1.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python
+
+# This prints "Hello, nurse!" to the screen.
+print "Hello, nurse!"
---
+++ day2.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+"""
+day2.py
+
+A first program! Hooraaaaay!
+"""
+print "Hello, nurse!"
